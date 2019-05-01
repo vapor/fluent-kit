ffa4b7188aaaf4191d9bdea4f8bbe0388ea2bafc
Tanner Nelson <me@tanner.xyz>
2019-04-30 21:04:51 -0400

    rm codablekit dep

--- Sources/FluentSQL/SQLQueryConverter.swift
+++ Sources/FluentSQL/SQLQueryConverter.swift
@@ -229,3 +229,28 @@ public struct SQLQueryConverter {
         }
     }
 }
+
+private struct StringCodingKey: CodingKey {
+    /// `CodingKey` conformance.
+    public var stringValue: String
+
+    /// `CodingKey` conformance.
+    public var intValue: Int? {
+        return Int(self.stringValue)
+    }
+
+    /// Creates a new `StringCodingKey`.
+    public init(_ string: String) {
+        self.stringValue = string
+    }
+
+    /// `CodingKey` conformance.
+    public init(stringValue: String) {
+        self.stringValue = stringValue
+    }
+
+    /// `CodingKey` conformance.
+    public init(intValue: Int) {
+        self.stringValue = intValue.description
+    }
+}
