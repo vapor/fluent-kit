b43cb94acc76a00959da2b9030d69860b0fa573b
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-12-10 15:39:27 -0600

    Don't set 'deleted_at' field when updating Trash model in FluentBenchmark.testUpdate test case

--- Sources/FluentBenchmark/FluentBenchmarker.swift
+++ Sources/FluentBenchmark/FluentBenchmarker.swift
@@ -119,7 +119,7 @@ public final class FluentBenchmarker {
             try checklist1.create(on: self.database).wait()
             try checklist2.create(on: self.database).wait()
 
-            let update = Trash(id: checklist1.id, contents: "Foo, Bar, Baz, Fizz, Buzz", deletedAt: deletedAt)
+            let update = Trash(id: checklist1.id, contents: "Foo, Bar, Baz, Fizz, Buzz")
             try update.update(on: self.database).wait()
 
             let trash = try Trash.query(on: self.database).filter(\.$contents == "Foo, Bar, Baz, Fizz, Buzz").all().wait()
