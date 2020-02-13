72d26653f335aa76576bc7ad2e42081df29e07e6
Natan Rolnik <natanrolnik@gmail.com>
2020-02-13 18:33:27 +0200

    Add isAttached and attachIfNeeded methods to Siblings (#151)
    * Add isAttached and attachIfNeeded methods to Siblings
    
    * Add Siblings.AttachMethod and address other PR comments

--- Sources/FluentKit/Properties/Siblings.swift
+++ Sources/FluentKit/Properties/Siblings.swift
@@ -2,6 +2,13 @@
 public final class Siblings<From, To, Through>: AnyProperty
     where From: Model, To: Model, Through: Model
 {
+    public enum AttachMethod {
+        // always create the pivot
+        case always
+
+        // only create the pivot if it doesn't already exist
+        case ifNotExists
+    }
 
     let from: KeyPath<Through, Parent<From>>
     let to: KeyPath<Through, Parent<To>>
@@ -34,7 +41,49 @@ public final class Siblings<From, To, Through>: AnyProperty
         return self
     }
 
+    // MARK: Checking state
+
+    public func isAttached(to: To, on database: Database) -> EventLoopFuture<Bool> {
+        guard let toID = to.id else {
+            fatalError("Cannot attach unsaved model.")
+        }
+
+        return self.isAttached(toID: toID, on: database)
+    }
+
+    public func isAttached(toID: To.IDValue, on database: Database) -> EventLoopFuture<Bool> {
+        guard let fromID = self.idValue else {
+            fatalError("Cannot check if siblings are attached to an unsaved model.")
+        }
+
+        return Through.query(on: database)
+            .filter(self.from.appending(path: \.$id) == fromID)
+            .filter(self.to.appending(path: \.$id) == toID)
+            .first()
+            .map { $0 != nil }
+    }
+
     // MARK: Operations
+
+    public func attach(
+        _ to: To,
+        method: AttachMethod,
+        on database: Database,
+        _ edit: @escaping (Through) -> () = { _ in }
+    ) -> EventLoopFuture<Void> {
+        switch method {
+        case .always:
+            return self.attach(to, on: database, edit)
+        case .ifNotExists:
+            return self.isAttached(to: to, on: database).flatMap { alreadyAttached in
+                if alreadyAttached {
+                    return database.eventLoop.makeSucceededFuture(())
+                }
+
+                return self.attach(to, on: database, edit)
+            }
+        }
+    }
 
     public func attach(
         _ to: To,
