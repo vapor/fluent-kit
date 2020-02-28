76293e7e1fe3b4221a6e87745582a2f4b186f3dd
tanner0101 <me@tanner.xyz>
2020-02-28 17:12:35 -0500

    updates

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-rc.1"),
     ],
     targets: [
         .target(name: "FluentKit", dependencies: [
