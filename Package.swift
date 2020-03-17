5e54f5d92d157db916d264df7904098f44af44ad
Gwynne Raskind <gwynne@darkrainfall.org>
2020-03-17 00:53:24 -0500

    Add async-kit dependency so we can use EventLoopFutureQueue.

--- Package.swift
+++ Package.swift
@@ -16,15 +16,18 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-rc.1"),
     ],
     targets: [
         .target(name: "FluentKit", dependencies: [
             .product(name: "NIO", package: "swift-nio"),
             .product(name: "Logging", package: "swift-log"),
+            .product(name: "AsyncKit", package: "async-kit"),
         ]),
         .target(name: "FluentBenchmark", dependencies: [
             .target(name: "FluentKit"),
             .target(name: "FluentSQL"),
+            .product(name: "AsyncKit", package: "async-kit"),
         ]),
         .target(name: "FluentSQL", dependencies: [
             .target(name: "FluentKit"),
