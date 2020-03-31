638232e98a9bcd2087c6fef22a0af178c1ba6dd1
Caleb Kleveter <caleb.kleveter@gmail.com>
2020-03-31 16:00:50 -0500

    Custom Timestamp Formats (#221)
    * Add custom value conversion to Field and Timestamp property wrappers
    
    * Added .testISO8601Timestamp test case to FluentBenchmarker
    
    * Fixed method call loop in AnyFieldValueConverter.decode(from:) method
    
    * Use field value converter to get correct new input value in Timestamp.touch(date:) method
    
    * Removed exlicet date comperisons from FluentKitBenchmark.testTimestamp_ISO8601
    
    Those assertions won't be correct because the ISO dates don't store the nanosecond information
    
    * Fixed call to timestamp tests in FluentKitBenchmarker
    
    * Hardcode output keys for ISO timestamps in FluentKitBenchmarker.testTimestamp_ISO8601
    
    * Decode ISO dates from events schema output instead of straight database output in FluentKitBenchmarks.testTimestamp_ISO8601
    
    * Removed NIO.ThreadSpecificVariable import from Timestamp.swift file
    
    * Remove basic Field value conversion
    
    * Removed added line to FieldProperty property definitions
    
    * Set field.inputValue to nil in TimestampProperty.output(from:) method
    
    * Only store Date type value in Timestamp.field.inputValue property
    
    * Gave TimestampFormatter an associated type Timestamp
    
    This allows for greater flexability in the ways a date can be formatted
    
    * Created DefaultTimestampFormatter to use Date as its Timestamp associated type
    
    * Removed need for Formatter generic parameter in TimestampProperty type
    
    * Use AnyTimestampFormatter.timestampType.init(from:) and .encode(to:) methods in TimestampProperty encode and decode methods
    
    * Made TimestampFormat non-generic
    
    * Made AnyTimestampFormatter.timestamp(from:) return type optional
    
    * Renamed AnyTimestampFormatter methods/properties to use anyTimestamp name
    
    * trigger ci
    
    * AnyTimestampFormatter.anyTimestamp(from:) return type is Optional<Codable>
    
    * Made UnixTimestampFormatter and DefaultTimestampFormatter struct private
    
    * We can't use DatabaseQuery.Value.null, we have to use .bind(nil) instead

--- Sources/FluentBenchmark/Tests/TimestampTests.swift
+++ Sources/FluentBenchmark/Tests/TimestampTests.swift
@@ -1,5 +1,12 @@
+@testable import class FluentKit.QueryBuilder
+
 extension FluentBenchmarker {
     public func testTimestamp() throws {
+        try self.testTimestamp_touch()
+        try self.testTimestamp_ISO8601()
+    }
+
+    public func testTimestamp_touch() throws {
         try runTest(#function, [
             UserMigration(),
         ]) {
@@ -15,6 +22,34 @@ extension FluentBenchmarker {
             XCTAssertNotNil(user.createdAt)
             XCTAssertNotNil(user.updatedAt)
             XCTAssertNotEqual(user.updatedAt, user.createdAt)
+        }
+    }
+
+    public func testTimestamp_ISO8601() throws {
+        try runTest(#function, [
+            EventMigration(),
+        ]) {
+            let event = Event(name: "ServerSide.swift")
+            try event.create(on: self.database).wait()
+
+            event.name = "Vapor Bay"
+            try event.save(on: self.database).wait()
+
+            let formatter = ISO8601DateFormatter()
+            let createdAt = try formatter.string(from: XCTUnwrap(event.createdAt))
+            let updatedAt = try formatter.string(from: XCTUnwrap(event.updatedAt))
+
+            try Event.query(on: self.database).run({ output in
+                do {
+                    let schema = output.schema("events")
+                    let createdAtField = try schema.decode(event.$createdAt.field.key, as: String.self)
+                    let updatedAtField = try schema.decode(event.$updatedAt.field.key, as: String.self)
+                    XCTAssertEqual(createdAtField, createdAt)
+                    XCTAssertEqual(updatedAtField, updatedAt)
+                } catch let error {
+                    XCTFail("Timestamp decoding from database output failed with error: \(error)")
+                }
+            }).wait()
         }
     }
 }
@@ -59,3 +94,43 @@ private struct UserMigration: Migration {
     }
 }
 
+
+private final class Event: Model {
+    static let schema = "events"
+
+    @ID(key: .id)
+    var id: UUID?
+
+    @Field(key: "name")
+    var name: String
+
+    @Timestamp(key: "created_at", on: .create, format: .iso8601)
+    var createdAt: Date?
+
+    @Timestamp(key: "updated_at", on: .update, format: .iso8601)
+    var updatedAt: Date?
+
+    init() { }
+
+    init(id: IDValue? = nil, name: String) {
+        self.id = id
+        self.name = name
+        self.createdAt = nil
+        self.updatedAt = nil
+    }
+}
+
+private struct EventMigration: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("events")
+            .field("id", .uuid, .identifier(auto: false))
+            .field("name", .string, .required)
+            .field("created_at", .string)
+            .field("updated_at", .string)
+            .create()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("events").delete()
+    }
+}
--- Sources/FluentKit/Properties/Timestamp.swift
+++ Sources/FluentKit/Properties/Timestamp.swift
@@ -1,3 +1,7 @@
+import class Foundation.ISO8601DateFormatter
+import class Foundation.DateFormatter
+import class NIO.ThreadSpecificVariable
+
 extension Fields {
     public typealias Timestamp = TimestampProperty<Self>
 }
@@ -14,6 +18,7 @@ public final class TimestampProperty<Model>
 {
     public let field: Model.OptionalField<Date>
     public let trigger: TimestampTrigger
+    let formatter: AnyTimestampFormatter
 
     public var projectedValue: TimestampProperty<Model> {
         return self
@@ -28,9 +33,14 @@ public final class TimestampProperty<Model>
         }
     }
 
-    public init(key: FieldKey, on trigger: TimestampTrigger) {
+    public convenience init(key: FieldKey, on trigger: TimestampTrigger) {
+        self.init(key: key, on: trigger, format: .default)
+    }
+
+    public init(key: FieldKey, on trigger: TimestampTrigger, format: TimestampFormat) {
         self.field = .init(key: key)
         self.trigger = trigger
+        self.formatter = format.formatter
     }
 
     public func touch(date: Date?) {
@@ -61,19 +71,35 @@ extension TimestampProperty: AnyProperty {
     }
     
     public func input(to input: inout DatabaseInput) {
-        self.field.input(to: &input)
+        let timestamp = self.value.flatMap { date in self.formatter.anyTimestamp(from: date) }
+        input.values[self.field.key] = .bind(timestamp ?? Optional<Date>.none)
     }
 
     public func output(from output: DatabaseOutput) throws {
-        try self.field.output(from: output)
+        self.field.inputValue = nil
+        guard output.contains(self.field.key) else { return }
+
+        do {
+            let date = try self.formatter.decode(at: self.field.key, from: output)
+            self.field.outputValue = date
+        } catch {
+            throw FluentError.invalidField(name: self.field.key.description, valueType: Value.self, error: error)
+        }
     }
 
     public func encode(to encoder: Encoder) throws {
-        try self.field.encode(to: encoder)
+        let timestamp = self.value.flatMap(self.formatter.anyTimestamp(from:)) ?? Optional<Date>.none
+        try timestamp.encode(to: encoder)
     }
 
     public func decode(from decoder: Decoder) throws {
-        try self.field.decode(from: decoder)
+        let container = try decoder.singleValueContainer()
+        if container.decodeNil() {
+            self.field.value = nil
+        } else {
+            let timestamp = try self.formatter.anyTimestamp.init(from: decoder)
+            self.field.inputValue = .bind(self.formatter.date(fromAny: timestamp))
+        }
     }
 }
 
@@ -129,4 +155,104 @@ extension Schema {
         let isFuture = DatabaseQuery.Filter.value(deletedAtField, .greaterThan, .bind(Date()))
         query.filters.append(.group([isNull, isFuture], .or))
     }
+}
+
+
+// MARK: - Timestamp Formatter
+
+public protocol AnyTimestampFormatter {
+    var anyTimestamp: Codable.Type { get }
+
+    func anyTimestamp(from date: Date) -> Codable?
+    func date(fromAny anyTimestamp: Codable) -> Date?
+
+    func decode(at key: FieldKey, from output: DatabaseOutput) throws -> Date?
+}
+
+public protocol TimestampFormatter: AnyTimestampFormatter {
+    associatedtype Timestamp: Codable
+
+    func timestamp(from date: Date) -> Timestamp?
+    func date(from timestamp: Timestamp) -> Date?
+}
+
+extension TimestampFormatter {
+    public var anyTimestamp: Codable.Type { Timestamp.self }
+
+
+    public func anyTimestamp(from date: Date) -> Codable? {
+        return self.timestamp(from: date) as Timestamp?
+    }
+
+    public func date(fromAny anyTimestamp: Codable) -> Date? {
+        return (anyTimestamp as? Timestamp).flatMap(self.date(from:))
+    }
+
+
+    public func decode(at key: FieldKey, from output: DatabaseOutput) throws -> Date? {
+        let timestamp = try output.decode(key, as: Timestamp?.self)
+        return timestamp.flatMap(self.date(from:))
+    }
+}
+
+
+extension DateFormatter: TimestampFormatter {
+    public func timestamp(from date: Date) -> String? { self.string(from: date) }
+}
+
+extension ISO8601DateFormatter: TimestampFormatter {
+    public func timestamp(from date: Date) -> String? { self.string(from: date) }
+}
+
+private struct UnixTimestampFormatter: TimestampFormatter {
+    func timestamp(from date: Date) -> Double? { date.timeIntervalSince1970 }
+    func date(from timestamp: Double) -> Date? { Date(timeIntervalSince1970: timestamp) }
+}
+
+private struct DefaultTimestampFormatter: TimestampFormatter {
+    func timestamp(from date: Date) -> Date? { date }
+    func date(from timestamp: Date) -> Date? { timestamp }
+}
+
+
+private final class TimestampFormatterCache {
+    static func formatter(for id: String, factory: () -> AnyTimestampFormatter) -> AnyTimestampFormatter {
+        if let formatter = self.current.currentValue?.cache[id] { return formatter }
+
+        let new = factory()
+
+        if self.current.currentValue == nil { self.current.currentValue = TimestampFormatterCache() }
+        self.current.currentValue?.cache[id] = new
+
+        return new
+    }
+
+    private static var current: ThreadSpecificVariable<TimestampFormatterCache> = .init()
+
+    var cache: [String: AnyTimestampFormatter] = [:]
+}
+
+
+public struct TimestampFormat {
+    public let id: String
+    private let factory: () -> AnyTimestampFormatter
+
+    public var formatter: AnyTimestampFormatter { TimestampFormatterCache.formatter(for: self.id, factory: self.factory) }
+
+    public init(_ id: String, formatter: @escaping () -> AnyTimestampFormatter) {
+        self.id = id
+        self.factory = formatter
+    }
+}
+
+extension TimestampFormat {
+    public static let iso8601 = TimestampFormat("iso8601", formatter: ISO8601DateFormatter.init)
+}
+
+extension TimestampFormat {
+    public static let unix = TimestampFormat("unix", formatter: UnixTimestampFormatter.init)
+}
+
+extension TimestampFormat {
+    public static let `default` = TimestampFormat("default", formatter: DefaultTimestampFormatter.init)
 }
