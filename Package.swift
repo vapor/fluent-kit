8de1431ab2cbe82bb57813f8b429b51537557f1e
Gwynne Raskind <gwynne@darkrainfall.org>
2020-05-15 02:40:31 -0500

    Fix typo in Package.swift. Now I have to wonder not only why was this working before without having the dependency at all, but why did it work for me locally in this broken form that definitely fails on Linux?

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-rc.1"),
-        .package(url:" https://github.com/vapor/async-kit.git", from: "1.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-rc.1"),
     ],
     targets: [
         .target(name: "FluentKit", dependencies: [
