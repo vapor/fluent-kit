e06b59ac3b990cc3f913a72e8d7f5cd3a310b114
tanner0101 <me@tanner.xyz>
2020-05-28 16:24:07 -0400

    add test for set to nil, fixes #284

--- Sources/FluentSQL/SQLQueryConverter.swift
+++ Sources/FluentSQL/SQLQueryConverter.swift
@@ -309,7 +309,11 @@ public struct SQLQueryConverter {
     private func value(_ value: DatabaseQuery.Value) -> SQLExpression {
         switch value {
         case .bind(let encodable):
-            return SQLBind(encodable)
+            if let optional = encodable as? AnyOptionalType, optional.wrappedValue == nil {
+                return SQLLiteral.null
+            } else {
+                return SQLBind(encodable)
+            }
         case .null:
             return SQLLiteral.null
         case .array(let values):
