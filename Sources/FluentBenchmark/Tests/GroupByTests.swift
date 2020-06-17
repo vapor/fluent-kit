b1b88bf50724f10be2bfe5a900c5f33318d65fe2
Caleb Kleveter <caleb.kleveter@gmail.com>
2020-06-17 15:11:23 -0500

    Only select planets.id column in FluentBenchmarker.testGroupBy_join test case

--- Sources/FluentBenchmark/Tests/GroupByTests.swift
+++ Sources/FluentBenchmark/Tests/GroupByTests.swift
@@ -10,12 +10,14 @@ extension FluentBenchmarker {
             let earth = try XCTUnwrap(Planet.query(on: self.database).filter(\.$name == "Earth").first().wait())
 
             let planets = try Planet.query(on: self.database)
+                .field(\.$id)
                 .join(PlanetTag.self, on: \PlanetTag.$planet.$id == \Planet.$id)
                 .filter(PlanetTag.self, \.$planet.$id == earth.requireID())
                 .all().wait()
             XCTAssertEqual(planets.count, 2)
 
             let groupedPlanets = try Planet.query(on: self.database)
+                .field(\.$id)
                 .join(PlanetTag.self, on: \PlanetTag.$planet.$id == \Planet.$id)
                 .filter(PlanetTag.self, \.$planet.$id == earth.requireID())
                 .group(by: \.$id)
