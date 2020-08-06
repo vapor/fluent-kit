3bcff797e5e9efab91209f2f2e064abc2c183042
Tanner <tannernelson@gmail.com>
2020-08-06 16:16:49 -0400

    Add raw query model decode helper (#362)
    * add raw model decode helper
    
    * add first() method
    
    * fix schema creation
    
    * add SQLRow.decode overload

---
+++ Sources/FluentBenchmark/Tests/SQLTests.swift
@@ -0,0 +1,104 @@
+import FluentSQL
+
+extension FluentBenchmarker {
+    public func testSQL() throws {
+        guard let sql = self.database as? SQLDatabase else {
+            return
+        }
+        try self.testSQL_rawDecode(sql)
+    }
+
+    private func testSQL_rawDecode(_ sql: SQLDatabase) throws {
+        try self.runTest(#function, [
+            UserMigration()
+        ]) {
+            let tanner = User(firstName: "Tanner", lastName: "Nelson", parentID: UUID())
+            try tanner.create(on: self.database).wait()
+
+            // test db.first(decoding:)
+            do {
+                let user = try sql.raw("SELECT * FROM users").first(decoding: User.self).wait()
+                XCTAssertNotNil(user)
+                if let user = user {
+                    XCTAssertEqual(user.id, tanner.id)
+                    XCTAssertEqual(user.firstName, tanner.firstName)
+                    XCTAssertEqual(user.lastName, tanner.lastName)
+                    XCTAssertEqual(user.$parent.id, tanner.$parent.id)
+                }
+            }
+
+            // test db.all(decoding:)
+            do {
+                let users = try sql.raw("SELECT * FROM users").all(decoding: User.self).wait()
+                XCTAssertEqual(users.count, 1)
+                if let user = users.first {
+                    XCTAssertEqual(user.id, tanner.id)
+                    XCTAssertEqual(user.firstName, tanner.firstName)
+                    XCTAssertEqual(user.lastName, tanner.lastName)
+                    XCTAssertEqual(user.$parent.id, tanner.$parent.id)
+                }
+            }
+
+            // test row.decode()
+            do {
+                let users = try sql.raw("SELECT * FROM users").all().wait().map {
+                    try $0.decode(model: User.self)
+                }
+                XCTAssertEqual(users.count, 1)
+                if let user = users.first {
+                    XCTAssertEqual(user.id, tanner.id)
+                    XCTAssertEqual(user.firstName, tanner.firstName)
+                    XCTAssertEqual(user.lastName, tanner.lastName)
+                    XCTAssertEqual(user.$parent.id, tanner.$parent.id)
+                }
+            }
+        }
+    }
+}
+
+
+private final class User: Model {
+    static let schema = "users"
+
+    @ID(key: .id)
+    var id: UUID?
+
+    @Field(key: "first_name")
+    var firstName: String
+
+    @Field(key: "last_name")
+    var lastName: String
+
+    @OptionalParent(key: "parent_id")
+    var parent: User?
+
+    init() { }
+
+    init(
+        id: UUID? = nil, 
+        firstName: String,
+        lastName: String,
+        parentID: UUID? = nil
+    ) {
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.$parent.id = parentID
+    }
+}
+
+private struct UserMigration: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("users")
+            .id()
+            .field("first_name", .string, .required)
+            .field("last_name", .string, .required)
+            .field("parent_id", .uuid)
+            .create()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("users").delete()
+    }
+}
+
---
+++ Sources/FluentSQL/SQLDatabase+Model.swift
@@ -0,0 +1,58 @@
+import SQLKit
+
+extension SQLQueryFetcher {
+    public func first<Model>(decoding model: Model.Type) -> EventLoopFuture<Model?> 
+        where Model: FluentKit.Model
+    {
+        self.all(decoding: Model.self).map { $0.first }
+    }
+
+    public func all<Model>(decoding model: Model.Type) -> EventLoopFuture<[Model]> 
+        where Model: FluentKit.Model
+    {
+        self.all().flatMapThrowing { rows in 
+            try rows.map { row in
+                let model = Model()
+                try model.output(from: SQLDatabaseOutput(sql: row))
+                return model
+            }
+        }
+    }
+}
+
+extension SQLRow {
+    public func decode<Model>(model: Model.Type) throws -> Model
+        where Model: FluentKit.Model
+    {
+        let model = Model()
+        try model.output(from: SQLDatabaseOutput(sql: self))
+        return model
+    }
+}
+
+private struct SQLDatabaseOutput: DatabaseOutput {
+    let sql: SQLRow
+
+    var description: String {
+        "\(self.sql)"
+    }
+
+    func schema(_ schema: String) -> DatabaseOutput {
+        self
+    }
+
+    func contains(_ key: FieldKey) -> Bool {
+        self.sql.contains(column: key.description)
+    }
+
+    func decodeNil(_ key: FieldKey) throws -> Bool {
+        try self.sql.decodeNil(column: key.description)
+    }
+
+    func decode<T>(_ key: FieldKey, as type: T.Type) throws -> T 
+        where T: Decodable
+    {
+        try self.sql.decode(column: key.description, as: T.self)
+    }
+}
+
