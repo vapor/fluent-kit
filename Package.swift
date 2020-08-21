8f723677a6deb77e0efa1f2af6ebb0d7c6e6f105
Caleb Kleveter <caleb.kleveter@gmail.com>
2020-08-21 16:42:53 -0500

    Minimum iOS version is 13 (#378)
    * Minimum iOS version is 10
    
    * Changed supported iOS platform to >=13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "fluent-kit",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v13)
     ],
     products: [
         .library(name: "FluentKit", targets: ["FluentKit"]),
