f0cc72ab101b88dfbdbe0b042da62386e1aa8195
Jaap Wijnen <JaapWijnen@users.noreply.github.com>
2020-10-26 15:58:45 +0100

    Add eager loading for pivots on sibling relationships (#391)
    * add eager loading for pivots on sibling relations
    
    * add test
    
    * fixes
    
    * add api docs
    
    * remove copied over line
    
    * Update Sources/FluentKit/Properties/Siblings.swift
    
    Co-authored-by: Siemen Sikkema <siemensikkema@users.noreply.github.com>
    
    Co-authored-by: Siemen Sikkema <siemensikkema@users.noreply.github.com>

--- Sources/FluentBenchmark/Tests/SiblingsTests.swift
+++ Sources/FluentBenchmark/Tests/SiblingsTests.swift
@@ -2,6 +2,7 @@ extension FluentBenchmarker {
     public func testSiblings() throws {
         try self.testSiblings_attach()
         try self.testSiblings_detachArray()
+        try self.testSiblings_pivotLoading()
     }
 
     private func testSiblings_attach() throws {
@@ -85,6 +86,19 @@ extension FluentBenchmarker {
                     .all().wait()
                 XCTAssertEqual(tags.count, 0)
             }
+        }
+    }
+    
+    private func testSiblings_pivotLoading() throws {
+        try self.runTest(#function, [
+            SolarSystem()
+        ]) {
+            let earth = try Planet.query(on: self.database)
+                .filter(\.$name == "Earth").with(\.$tags).with(\.$tags.$pivots)
+                .first().wait()!
+            
+            // verify tag count
+            XCTAssertEqual(earth.$tags.pivots.count, 2)
         }
     }
 }
--- Sources/FluentKit/Properties/Siblings.swift
+++ Sources/FluentKit/Properties/Siblings.swift
@@ -22,6 +22,17 @@ public final class SiblingsProperty<From, To, Through>
     var idValue: From.IDValue?
     
     public var value: [To]?
+    
+    /// Allows eager loading of pivot objects through the sibling relation.
+    /// Example:
+    ///
+    ///     Planet.query(on: db)
+    ///         .with(\.$tags).with(\.$tags.$pivots).first() { planet in
+    ///             // you can now access the loaded pivots using:
+    ///             let pivots = planet.$tags.pivots
+    ///         }
+    @ChildrenProperty<From, Through>
+    public var pivots: [Through]
 
     public init(
         through _: Through.Type,
@@ -30,6 +41,7 @@ public final class SiblingsProperty<From, To, Through>
     ) {
         self.from = from
         self.to = to
+        self._pivots = ChildrenProperty<From, Through>(for: from)
     }
 
     public var wrappedValue: [To] {
@@ -207,6 +219,7 @@ extension SiblingsProperty: AnyDatabaseProperty {
         let key = From()._$id.key
         if output.contains(key) {
             self.idValue = try output.decode(key, as: From.IDValue.self)
+            self._pivots.idValue = self.idValue
         }
     }
 }
