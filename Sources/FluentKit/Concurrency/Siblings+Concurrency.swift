39fc711f4910bc7cba4c66cf98cd165e61b2a085
Tim <0xtimc@gmail.com>
2021-04-28 13:39:36 +0100

    Add async APIs for Siblings

---
+++ Sources/FluentKit/Concurrency/Siblings+Concurrency.swift
@@ -0,0 +1,53 @@
+#if compiler(>=5.5) && $AsyncAwait
+ import _NIOConcurrency
+
+ @available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+ public extension SiblingsProperty {
+    // MARK: Checking state
+
+    func isAttached(to: To, on database: Database) async throws -> Bool {
+        try await self.isAttached(to: to, on: database).get()
+    }
+
+    func isAttached(toID: To.IDValue, on database: Database) async throws -> Bool {
+        try await self.isAttached(toID: toID, on: database).get()
+    }
+
+    // MARK: Operations
+
+    func attach(
+        _ tos: [To],
+        on database: Database,
+        _ edit: (Through) -> () = { _ in }
+    ) async throws {
+        try await self.attach(tos, on: database, edit).get()
+    }
+
+    func attach(
+        _ to: To,
+        method: AttachMethod,
+        on database: Database,
+        _ edit: @escaping (Through) -> () = { _ in }
+    ) async throws {
+        try await self.attach(to, method: method, on: database, edit).get()
+    }
+
+    func attach(
+        _ to: To,
+        on database: Database,
+        _ edit: (Through) -> () = { _ in }
+    ) async throws {
+        try await self.attach(to, on: database, edit).get()
+    }
+
+
+    func detach(_ tos: [To], on database: Database) async throws {
+        try await self.detach(tos, on: database).get()
+    }
+
+    func detach(_ to: To, on database: Database) async throws {
+        try await self.detach(to, on: database).get()
+    }
+ }
+
+ #endif
