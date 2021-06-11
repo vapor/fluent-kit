0feb6c06337a0ff1a070bafecd9efba018ef73a4
Max Howell <mxcl@me.com>
2021-06-11 16:07:46 -0400

    Make these closures async for desired usage

--- Sources/FluentKit/Concurrency/Database+Concurrency.swift
+++ Sources/FluentKit/Concurrency/Database+Concurrency.swift
@@ -3,12 +3,20 @@ import _NIOConcurrency
 
 @available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public extension Database {
-    func transaction<T>(_ closure: @escaping (Database) -> EventLoopFuture<T>) async throws -> T {
-        try await self.transaction(closure).get()
+    func transaction<T>(_ closure: @escaping (Database) async throws -> T) async throws -> T {
+        try await self.transaction { db -> EventLoopFuture<T> in
+            let promise = self.eventLoop.makePromise(of: T.self)
+            promise.completeWithAsync{ try await closure(db) }
+            return promise.futureResult
+        }.get()
     }
 
-    func withConnection<T>(_ closure: @escaping (Database) -> EventLoopFuture<T>) async throws -> T {
-        try await self.withConnection(closure).get()
+    func withConnection<T>(_ closure: @escaping (Database) async throws -> T) async throws -> T {
+        try await self.withConnection { db -> EventLoopFuture<T> in
+            let promise = self.eventLoop.makePromise(of: T.self)
+            promise.completeWithAsync{ try await closure(db) }
+            return promise.futureResult
+        }.get()
     }
 }
 
