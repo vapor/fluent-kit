b0f80a5ca8bd506051f070a037c5fecc9845617b
Taenerys <thuanh99@student.fdu.edu>
2021-07-29 20:21:37 -0700

    Added document comments to Migration protocol

--- Sources/FluentKit/Migration/Migration.swift
+++ Sources/FluentKit/Migration/Migration.swift
@@ -1,6 +1,19 @@
+/// `Migration` can assist with database migration. It can choose
+/// to create a new database schema for migration, or drop an
+/// existing schema. These actions are done in-place, returning no
+/// values.
 public protocol Migration {
     var name: String { get }
+    /// Prepares new database schema for the migration.
+    /// - Parameters:
+    ///     - database: `Database` to be created.
+    /// - returns: An asynchronous `Void`.
     func prepare(on database: Database) -> EventLoopFuture<Void>
+    
+    /// Drops the database.
+    /// - Parameters:
+    ///     - database: `Database` to be dropped.
+    /// - returns: An asynchronous `Void`.
     func revert(on database: Database) -> EventLoopFuture<Void>
 }
 
