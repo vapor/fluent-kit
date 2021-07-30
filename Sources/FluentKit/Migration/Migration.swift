e63ef55e38b0f7efce6cba18ce8e2b5568834fec
Taenerys <thuanh99@student.fdu.edu>
2021-07-30 12:04:35 -0700

    Made changes as requested

--- Sources/FluentKit/Migration/Migration.swift
+++ Sources/FluentKit/Migration/Migration.swift
@@ -1,18 +1,21 @@
-/// `Migration` can assist with database migration. It can choose
-/// to create a new database schema for migration, or drop an
-/// existing schema. These actions are done in-place, returning no
-/// values.
+/// Fluent's `Migration` can handle database migrations, which can include
+/// adding new table, changing existing tables or adding
+/// seed data. These actions are executed only once.
 public protocol Migration {
+    
+    /// The name of the migration which Fluent uses to track the state of.
     var name: String { get }
-    /// Prepares new database schema for the migration.
+    
+    /// Called when a migration is executed.
     /// - Parameters:
-    ///     - database: `Database` to be created.
+    ///     - database: `Database` to run the migration on,
     /// - returns: An asynchronous `Void`.
+    
     func prepare(on database: Database) -> EventLoopFuture<Void>
     
-    /// Drops the database.
+    /// Called when the changes from a migration are reverted.
     /// - Parameters:
-    ///     - database: `Database` to be dropped.
+    ///     - database: `Database` to revert the migration on.
     /// - returns: An asynchronous `Void`.
     func revert(on database: Database) -> EventLoopFuture<Void>
 }
