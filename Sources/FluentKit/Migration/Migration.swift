95ce7c07c52ecaf4d9258191857c7405f0cae357
Anh Pham <thuanhsone99@gmail.com>
2021-08-03 09:33:51 -0700

    Added documentation comments to `Migration` (#445)
    * Added document comments to Migration protocol
    
    * Made changes as requested

--- Sources/FluentKit/Migration/Migration.swift
+++ Sources/FluentKit/Migration/Migration.swift
@@ -1,6 +1,22 @@
+/// Fluent's `Migration` can handle database migrations, which can include
+/// adding new table, changing existing tables or adding
+/// seed data. These actions are executed only once.
 public protocol Migration {
+    
+    /// The name of the migration which Fluent uses to track the state of.
     var name: String { get }
+    
+    /// Called when a migration is executed.
+    /// - Parameters:
+    ///     - database: `Database` to run the migration on,
+    /// - returns: An asynchronous `Void`.
+    
     func prepare(on database: Database) -> EventLoopFuture<Void>
+    
+    /// Called when the changes from a migration are reverted.
+    /// - Parameters:
+    ///     - database: `Database` to revert the migration on.
+    /// - returns: An asynchronous `Void`.
     func revert(on database: Database) -> EventLoopFuture<Void>
 }
 
