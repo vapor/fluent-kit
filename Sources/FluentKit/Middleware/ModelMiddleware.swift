54ec01cd3999eaf155f66cedea90f9a2b5b7608e
Elizaveta Diuchkova <dyuchkovae@gmail.com>
2021-08-10 14:35:39 +0200

    added implementation of async/await handle method for ModelMiddleware

--- Sources/FluentKit/Middleware/ModelMiddleware.swift
+++ Sources/FluentKit/Middleware/ModelMiddleware.swift
@@ -54,6 +54,28 @@ extension ModelMiddleware {
         }
     }
     
+    #if compiler(>=5.5)
+    @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+    public func handle(_ event: ModelEvent, _ model: AnyModel, on db: Database, chainingTo next: AnyModelResponder) async throws {
+        guard let modelType = model as? Model else {
+            return try await next.handle(event, model, on: db).get()
+        }
+        
+        switch event {
+        case .create:
+            return try await create(model: modelType, on: db, next: next)
+        case .update:
+            return try await update(model: modelType, on: db, next: next)
+        case .delete(let force):
+            return try await delete(model: modelType, force: force, on: db, next: next)
+        case .softDelete:
+            return try await softDelete(model: modelType, on: db, next: next)
+        case .restore:
+            return try await restore(model: modelType, on: db, next: next)
+        }
+    }
+    #endif
+    
     public func create(model: Model, on db: Database, next: AnyModelResponder) -> EventLoopFuture<Void> {
         return next.create(model, on: db)
     }
