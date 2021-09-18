176f2b4b7c35a89dd756a5d6d8d5a8473bf92410
Gwynne Raskind <gwynne@darkrainfall.org>
2021-09-18 09:06:14 -0500

    Use `Swift._typeName()` instead of `String(reflecting:)` in `Migration.defaultName` for a huge performance improvement. (#458)

--- Sources/FluentKit/Migration/Migration.swift
+++ Sources/FluentKit/Migration/Migration.swift
@@ -26,6 +26,20 @@ extension Migration {
     }
 
     internal var defaultName: String {
+#if compiler(>=5.3) && compiler(<6)
+        /// `String.init(reflecting:)` creates a `Mirror` unconditionally, but
+        /// when the parameter is a metatype (such as is the case here), that
+        /// mirror is never actually used for anything. Unfortunately, just
+        /// creating it slows this accessor down by at least 30% at best, and
+        /// as much as 50% in some cases. Given that it's already incorrect to
+        /// be depending on the stability of `String(reflecting:)`'s output
+        /// anyway, it seems to make more sense to just call the underlying
+        /// runtime function directly instead of taking the huge speed hit just
+        /// because the leading underscore makes it harder to ignore the
+        /// fragility of the usage.
+        return Swift._typeName(Self.self, qualified: true)
+#else
         return String(reflecting: Self.self)
+#endif
     }
 }
