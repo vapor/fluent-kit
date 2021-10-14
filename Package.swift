fb601f37c0f54c80ab0ab46a2105d657ac5fb3e5
Jared Henderson <jared@netrivet.com>
2021-10-14 08:26:08 -0400

    update swiftnio for compat with Xcode 13 GM

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "XCTFluent", targets: ["XCTFluent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.1.0"),
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.4.0"),
