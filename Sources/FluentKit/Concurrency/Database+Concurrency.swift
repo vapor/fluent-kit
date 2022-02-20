4aea8c54798999e75d1f6c832f96ab1700faf981
Gwynne Raskind <gwynne@darkrainfall.org>
2022-02-20 10:50:16 -0600

    Add missing @Sendable specifier to closure parameters of Database.transaction() and Database.withConnection()

--- Sources/FluentKit/Concurrency/Database+Concurrency.swift
+++ Sources/FluentKit/Concurrency/Database+Concurrency.swift
@@ -3,7 +3,7 @@ import NIOCore
 
 @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 public extension Database {
-    func transaction<T>(_ closure: @escaping (Database) async throws -> T) async throws -> T {
+    func transaction<T>(_ closure: @Sendable @escaping (Database) async throws -> T) async throws -> T {
         try await self.transaction { db -> EventLoopFuture<T> in
             let promise = self.eventLoop.makePromise(of: T.self)
             promise.completeWithTask{ try await closure(db) }
@@ -11,7 +11,7 @@ public extension Database {
         }.get()
     }
 
-    func withConnection<T>(_ closure: @escaping (Database) async throws -> T) async throws -> T {
+    func withConnection<T>(_ closure: @Sendable @escaping (Database) async throws -> T) async throws -> T {
         try await self.withConnection { db -> EventLoopFuture<T> in
             let promise = self.eventLoop.makePromise(of: T.self)
             promise.completeWithTask{ try await closure(db) }
