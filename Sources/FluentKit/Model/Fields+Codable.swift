509f60f9b20606e64b18415f5fc5d0893fa11265
Gwynne Raskind <gwynne@vapor.codes>
2022-08-10 07:11:26 -0500

    Still need a wrapping Decoder for decoding Fields because bad design of everything ever. Make printouts of arbitrary coding keys a bit easier to read.

--- Sources/FluentKit/Model/Fields+Codable.swift
+++ Sources/FluentKit/Model/Fields+Codable.swift
@@ -5,8 +5,10 @@ extension Fields {
         let container = try decoder.container(keyedBy: MissingStdlibAPICodingKey.self)
         
         for (key, property) in self.codableProperties {
+            let propDecoder = AirQuotesSafeSuperDecoder(container: container, key: key)
+            
             do {
-                try property.decode(from: container.superDecoder(forKey: key))
+                try property.decode(from: propDecoder)
             } catch {
                 throw DecodingError.typeMismatch(type(of: property).anyValueType, .init(
                     codingPath: container.codingPath + [key],
@@ -40,4 +42,26 @@ internal struct MissingStdlibAPICodingKey: CodingKey, Hashable {
   internal let stringValue: String, intValue: Int?
   internal init(stringValue: String) { (self.stringValue, self.intValue) = (stringValue, Int(stringValue)) }
   internal init(intValue: Int) { (self.stringValue, self.intValue) = ("\(intValue)", intValue) }
+  internal var description: String { "GenericCodingKey(\"\(self.stringValue)\"\(self.intValue.map { ", int: \($0)" } ?? ""))" }
+}
+
+/// This type's only purpose is to compensate for the changed behavior of `.superDecoder(forKey:)`, which used
+/// to return a `Decoder` representing a `NULL` value when the key was not present, but since 5.5 on Linux (and
+/// possibly eventually on macOS as well) will throw a key not found error for missing keys. Therefore, this
+/// `Decoder` wraps the container from which the key is supposed to come and allows an additional level of
+/// descent into the decode before an error would be thrown.
+///
+/// It is really unfortunate to need this; it's no win for peformance, for one. It's just the least unpleasant
+/// alternative available as long as the overall semantics remain undisturbed.
+private struct AirQuotesSafeSuperDecoder<K: CodingKey>: Decoder, SingleValueDecodingContainer {
+    var codingPath: [CodingKey] { self.container.codingPath }
+    var userInfo: [CodingUserInfoKey: Any] { [:] }
+    let container: KeyedDecodingContainer<K>
+    let key: K
+
+    func container<NK: CodingKey>(keyedBy: NK.Type) throws -> KeyedDecodingContainer<NK> { try self.container.nestedContainer(keyedBy: NK.self, forKey: self.key) }
+    func unkeyedContainer() throws -> UnkeyedDecodingContainer { try self.container.nestedUnkeyedContainer(forKey: self.key) }
+    func singleValueContainer() throws -> SingleValueDecodingContainer { self }
+    func decode<T: Decodable>(_: T.Type) throws -> T { try self.container.decode(T.self, forKey: self.key) }
+    func decodeNil() -> Bool { self.container.contains(self.key) ? try! self.container.decodeNil(forKey: self.key) : true }
 }
