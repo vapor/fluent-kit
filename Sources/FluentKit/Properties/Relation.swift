971e08b5f71f3e1ddc5e01c6bb39c419b5cba400
Gwynne Raskind <gwynne@vapor.codes>
2023-02-18 13:54:28 -0600

    Add documentation comments to Relation

--- Sources/FluentKit/Properties/Relation.swift
+++ Sources/FluentKit/Properties/Relation.swift
@@ -1,3 +1,8 @@
+/// A protocol which designates a conforming type as representing a database relation of any kind. Intended
+/// for use only by FluentKit property wrappers.
+///
+/// - Note: This protocol should probably require conformance to ``Property``, but adding that requirement
+///   wouldn't have enough value to be worth having to hand-wave a technically semver-major change.
 public protocol Relation {
     associatedtype RelatedValue
     var name: String { get }
@@ -6,12 +11,24 @@ public protocol Relation {
 }
 
 extension Relation {
+    /// Return the value of the relation, loading it first if necessary.
+    ///
+    /// If the value is loaded (including reloading), the value is set in the property before being returned.
+    ///
+    /// - Note: This API is strongly preferred over ``Relation/load(on:)``, even when the caller does not need
+    ///   the returned value, in order to minimize unnecessary database traffic.
+    ///
+    /// - Parameters:
+    ///   - reload: If `true`, load the value from the database unconditionally, overwriting any previously
+    ///     loaded value.
+    ///   - database: The database to use if the value needs to be loaded.
+    /// - Returns: The loaded value.
     public func get(reload: Bool = false, on database: Database) -> EventLoopFuture<RelatedValue> {
         if let value = self.value, !reload {
             return database.eventLoop.makeSucceededFuture(value)
         } else {
             return self.load(on: database).flatMapThrowing {
-                guard let value = self.value else {
+                guard let value = self.value else { // This should never actually happen, but just in case...
                     throw FluentError.relationNotLoaded(name: self.name)
                 }
                 return value
