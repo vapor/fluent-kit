2a82c298e31162fe714be2bdf6f738ae0c827ef9
Tim <0xtimc@gmail.com>
2023-02-24 01:31:33 +0000

    Bring back exports

--- Sources/FluentKit/Exports.swift
+++ Sources/FluentKit/Exports.swift
@@ -1,14 +1,14 @@
 #if !BUILDING_DOCC
 
-//@_exported import struct Foundation.Date
-//@_exported import struct Foundation.UUID
-//
-//@_exported import Logging
-//
-//@_exported import protocol NIO.EventLoop
-//@_exported import class NIO.EventLoopFuture
-//@_exported import struct NIO.EventLoopPromise
-//@_exported import protocol NIO.EventLoopGroup
-//@_exported import class NIO.NIOThreadPool
+@_exported import struct Foundation.Date
+@_exported import struct Foundation.UUID
+
+@_exported import Logging
+
+@_exported import protocol NIO.EventLoop
+@_exported import class NIO.EventLoopFuture
+@_exported import struct NIO.EventLoopPromise
+@_exported import protocol NIO.EventLoopGroup
+@_exported import class NIO.NIOThreadPool
 
 #endif
