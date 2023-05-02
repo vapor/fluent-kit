b00d8af2c05fb50c80074182d53944a167f512a4
Gwynne Raskind <gwynne@vapor.codes>
2023-05-02 06:36:52 -0500

    Update exports to use the new attribute in Swift 5.8

--- Sources/FluentBenchmark/Exports.swift
+++ Sources/FluentBenchmark/Exports.swift
@@ -1,4 +1,9 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import FluentKit
+@_documentation(visibility: internal) @_exported import XCTest
+
+#elseif !BUILDING_DOCC
 
 @_exported import FluentKit
 @_exported import XCTest
