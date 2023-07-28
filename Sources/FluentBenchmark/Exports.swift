9c0ec5771b9adda9dbef273f0405d68adac133d9
Gwynne Raskind <gwynne@vapor.codes>
2023-07-27 23:24:47 -0500

    Fixup Exports.swift

--- Sources/FluentBenchmark/Exports.swift
+++ Sources/FluentBenchmark/Exports.swift
@@ -3,7 +3,7 @@
 @_documentation(visibility: internal) @_exported import FluentKit
 @_documentation(visibility: internal) @_exported import XCTest
 
-#elseif !BUILDING_DOCC
+#else
 
 @_exported import FluentKit
 @_exported import XCTest
--- Sources/FluentSQL/Exports.swift
+++ Sources/FluentSQL/Exports.swift
@@ -3,7 +3,7 @@
 @_documentation(visibility: internal) @_exported import FluentKit
 @_documentation(visibility: internal) @_exported import SQLKit
 
-#elseif !BUILDING_DOCC
+#else
 
 @_exported import FluentKit
 @_exported import SQLKit
