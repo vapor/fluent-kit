024c360935ea5f3097b5325b540b3da7e7c63375
Adam Fowler <adamfowler71@gmail.com>
2023-12-26 07:34:46 +0100

    Make DatabaseID Sendable

--- Sources/FluentKit/Database/DatabaseID.swift
+++ Sources/FluentKit/Database/DatabaseID.swift
@@ -1,4 +1,4 @@
-public struct DatabaseID: Hashable, Codable {
+public struct DatabaseID: Hashable, Codable, Sendable {
     public let string: String
     public init(string: String) {
         self.string = string
