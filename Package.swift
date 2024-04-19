a11f4bb16cdd089bed8d2664b64d9abca69461f5
Gwynne Raskind <gwynne@vapor.codes>
2024-04-19 16:50:11 -0500

    Bump min Swift to 5.8, add feature flags

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.8
 import PackageDescription
 
 let package = Package(
@@ -22,29 +22,60 @@ let package = Package(
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.17.0"),
     ],
     targets: [
-        .target(name: "FluentKit", dependencies: [
-            .product(name: "NIO", package: "swift-nio"),
-            .product(name: "NIOCore", package: "swift-nio"),
-            .product(name: "Logging", package: "swift-log"),
-            .product(name: "AsyncKit", package: "async-kit"),
-            .product(name: "SQLKit", package: "sql-kit"),
-        ]),
-        .target(name: "FluentBenchmark", dependencies: [
-            .target(name: "FluentKit"),
-            .target(name: "FluentSQL"),
-        ]),
-        .target(name: "FluentSQL", dependencies: [
-            .target(name: "FluentKit"),
-            .product(name: "SQLKit", package: "sql-kit"),
-        ]),
-        .target(name: "XCTFluent", dependencies: [
-            .target(name: "FluentKit"),
-            .product(name: "NIOEmbedded", package: "swift-nio"),
-        ]),
-        .testTarget(name: "FluentKitTests", dependencies: [
-            .target(name: "FluentBenchmark"),
-            .target(name: "FluentSQL"),
-            .target(name: "XCTFluent"),
-        ]),
+        .target(
+            name: "FluentKit",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "AsyncKit", package: "async-kit"),
+                .product(name: "SQLKit", package: "sql-kit"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .target(
+            name: "FluentBenchmark",
+            dependencies: [
+                .target(name: "FluentKit"),
+                .target(name: "FluentSQL"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .target(
+            name: "FluentSQL",
+            dependencies: [
+                .product(name: "SQLKit", package: "sql-kit"),
+                .target(name: "FluentKit"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .target(
+            name: "XCTFluent",
+            dependencies: [
+                .product(name: "NIOEmbedded", package: "swift-nio"),
+                .target(name: "FluentKit"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "FluentKitTests",
+            dependencies: [
+                .target(name: "FluentBenchmark"),
+                .target(name: "FluentSQL"),
+                .target(name: "XCTFluent"),
+            ],
+            swiftSettings: swiftSettings
+        ),
     ]
 )
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ConciseMagicFile"),
+    .enableUpcomingFeature("ForwardTrailingClosures"),
+    .enableUpcomingFeature("ImportObjcForwardDeclarations"),
+    .enableUpcomingFeature("DisableOutwardActorInference"),
+    .enableUpcomingFeature("IsolatedDefaultValues"),
+    .enableUpcomingFeature("GlobalConcurrency"),
+    .enableUpcomingFeature("StrictConcurrency"),
+    .enableExperimentalFeature("StrictConcurrency=complete"),
+] }
--- Sources/FluentKit/Exports.swift
+++ Sources/FluentKit/Exports.swift
@@ -1,27 +1,10 @@
-#if swift(>=5.8)
-
 @_documentation(visibility: internal) @_exported import struct Foundation.Date
 @_documentation(visibility: internal) @_exported import struct Foundation.UUID
 
 @_documentation(visibility: internal) @_exported import Logging
 
-@_documentation(visibility: internal) @_exported import protocol NIO.EventLoop
-@_documentation(visibility: internal) @_exported import class NIO.EventLoopFuture
-@_documentation(visibility: internal) @_exported import struct NIO.EventLoopPromise
-@_documentation(visibility: internal) @_exported import protocol NIO.EventLoopGroup
-@_documentation(visibility: internal) @_exported import class NIO.NIOThreadPool
-
-#else
-
-@_exported import struct Foundation.Date
-@_exported import struct Foundation.UUID
-
-@_exported import Logging
-
-@_exported import protocol NIO.EventLoop
-@_exported import class NIO.EventLoopFuture
-@_exported import struct NIO.EventLoopPromise
-@_exported import protocol NIO.EventLoopGroup
-@_exported import class NIO.NIOThreadPool
-
-#endif
+@_documentation(visibility: internal) @_exported import protocol NIOCore.EventLoop
+@_documentation(visibility: internal) @_exported import class NIOCore.EventLoopFuture
+@_documentation(visibility: internal) @_exported import struct NIOCore.EventLoopPromise
+@_documentation(visibility: internal) @_exported import protocol NIOCore.EventLoopGroup
+@_documentation(visibility: internal) @_exported import class NIOPosix.NIOThreadPool
