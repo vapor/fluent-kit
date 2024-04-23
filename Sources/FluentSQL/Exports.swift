57e65764696692f357a66ff99b6dc51a9ee769ef
Gwynne Raskind <gwynne@vapor.codes>
2024-04-23 00:18:19 -0500

    Remove old compiler condition

--- Sources/FluentSQL/Exports.swift
+++ Sources/FluentSQL/Exports.swift
@@ -1,11 +1,2 @@
-#if swift(>=5.8)
-
 @_documentation(visibility: internal) @_exported import FluentKit
 @_documentation(visibility: internal) @_exported import SQLKit
-
-#else
-
-@_exported import FluentKit
-@_exported import SQLKit
-
-#endif
