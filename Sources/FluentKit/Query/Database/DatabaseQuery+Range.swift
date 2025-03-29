258b79df9688f997ee4bdd575fea0deb74d34388
Gwynne Raskind <gwynne@vapor.codes>
2025-03-29 02:27:41 -0500

    Deprecate DatabaseQuery.Limit.custom(_:) and DatabaseQuery.Offset.custom(_:), as using them always causes a fatalError()

--- Sources/FluentKit/Query/Database/DatabaseQuery+Range.swift
+++ Sources/FluentKit/Query/Database/DatabaseQuery+Range.swift
@@ -1,11 +1,21 @@
 extension DatabaseQuery {
     public enum Limit: Sendable {
         case count(Int)
+
+        /// Due to design limitations in SQLKit, it is not possible to provide a custom limit expression;
+        /// as such, it is in turn a design flaw of FluentKit that this enum exists at all. Any attempt to
+        /// use ``custom(_:)`` in a query unconditionally triggers a fatal error at runtime.
+        @available(*, deprecated, message: "`DatabaseQuery.Limit.custom(_:)` does not work.")
         case custom(any Sendable)
     }
 
     public enum Offset: Sendable {
         case count(Int)
+
+        /// Due to design limitations in SQLKit, it is not possible to provide a custom offset expression;
+        /// as such, it is in turn a design flaw of FluentKit that this enum exists at all. Any attempt to
+        /// use ``custom(_:)`` in a query unconditionally triggers a fatal error at runtime.
+        @available(*, deprecated, message: "`DatabaseQuery.Offset.custom(_:)` triggers a fatal error when used.")
         case custom(any Sendable)
     }
 }
