642ac23b3e2848868f8aa5ed805614bb0383af3c
Paul Toffoloni <paultoffoloni@gmail.com>
2026-01-12 15:41:54 +0100

    Use custom fluent span attributes

--- Sources/FluentKit/Query/Database/DatabaseQuery.swift
+++ Sources/FluentKit/Query/Database/DatabaseQuery.swift
@@ -35,18 +35,12 @@ public struct DatabaseQuery: Sendable {
 
     func withTracing<T>(_ closure: () async throws -> T) async rethrows -> T {
         if shouldTrace {
-            try await withSpan("db.query", context: self.serviceContext, ofKind: .client) { span in
-                // https://opentelemetry.io/docs/specs/semconv/database/database-spans/#span-definition
-                // We add what we can. The rest is up to the underlying driver packages
+            try await withSpan("fluent.query", context: self.serviceContext, ofKind: .client) { span in
                 span.updateAttributes { attributes in
-                    // db.system.name
-                    attributes["db.collection.name"] = self.schema
-                    attributes["db.namespace"] = self.space
-                    attributes["db.operation.name"] = "\(self.action)"
-                    // db.response.status_code
-                    // error.type
-                    // server.port
-                    attributes["db.query.summary"] = "\(self.action) \(self.space.map { "\($0)." } ?? "")\(self.schema)"
+                    attributes["fluent.query.collection"] = self.schema
+                    attributes["fluent.query.namespace"] = self.space
+                    attributes["fluent.query.operation"] = "\(self.action)"
+                    attributes["fluent.query.summary"] = "\(self.action) \(self.space.map { "\($0)." } ?? "")\(self.schema)"
                 }
                 return try await closure()
             }
